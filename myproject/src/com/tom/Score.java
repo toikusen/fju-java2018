a2c3b246a6b684e88e92eb96baa3580a5a2bd0de
toikusen <tuyucheng0407@gmail.com>
2018-11-27 12:06:50 +0800

    score

--- myproject/src/com/tom/Score.java
+++ myproject/src/com/tom/Score.java
@@ -21,4 +21,63 @@ public class Score {
 		int score = (english+math)/2;
 		return score;
 	}
+	public void print() {
+		int average = getAverage();
+		System.out.print(name + "\t" + english + "\t" + math + "\t");
+		System.out.print(average + "\t" + highest() + "\t" +
+				((average < 60) ? "FAILED" : "PASS"));
+		
+		
+		System.out.println("\t"+getGrading());
+	}
+	
+	public char getGrading() {
+		char grading = 'F';
+		int average = getAverage();
+		switch(average/10) {
+		case 10:
+		case 9:
+			grading = 'A';
+			break;
+		case 8:
+			grading = 'B';
+			break;
+		case 7:
+			grading = 'C';
+			break;
+		case 6:
+			grading = 'D';
+			break;
+		default:
+			grading = 'F';
+				
+		}
+		
+		
+		/*
+		
+		if (average >= 90 && average <= 100) {
+			grading = 'A';
+		} else if (average >= 80 && average <= 89) {
+			grading = 'B';
+		} else if (average >= 70 && average <= 79) {
+			grading = 'C';
+		} else if (average >= 60 && average <= 69) {
+			grading = 'D';
+		}
+		*/
+		return grading;
+	}
+
+	public int highest() {
+		if (english > math) {
+			return english;
+		} else {
+			return math;
+		}
+	}
+
+	public int getAverage() {
+		return (english + math) / 2;
+	}
 }
