227e491fab3fb4318dc9e5048b5c04f616cde38c
user <user@es301n18>
2019-02-26 11:59:41 +0800

    123456

--- myproject/src/com/tom/aaa/Tester.java
+++ myproject/src/com/tom/aaa/Tester.java
@@ -5,7 +5,7 @@ import com.tom.Student;
 public class Tester {
 
 	public static void main(String[] args) {
-		Student stu = new Student(Maxim,88);
+		Student stu = new Student("Maxim",88);
 		stu.setEnglish(100);
 
 	}
