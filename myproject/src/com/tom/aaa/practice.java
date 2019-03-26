97209a39de823c7a54ff3f4d3d714d5778aa5e78
user <user@es301n18>
2019-03-26 12:05:30 +0800

    shopping

---
+++ myproject/src/com/tom/aaa/practice.java
@@ -0,0 +1,28 @@
+package com.tom.aaa;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class practice {
+
+	public static void main(String[] args) {
+		HashMap<String,Integer> item = new HashMap<>();
+		Scanner scanner = new Scanner(System.in);
+		
+		String name = "";
+		while (! name.equals("q")) {
+			System.out.print("Please enter item name:(enter q:break)");
+			name = scanner.nextLine();
+			if (name == "q") {
+				break;
+			}
+			System.out.print("Please enter number:");
+			Integer number = Integer.parseInt(scanner.nextLine());
+			item.put(name, number);
+			System.out.println(item);	
+			
+			}
+			 	
+		}
+	
+}
